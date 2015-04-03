de4f8b118a7b1b19aa3ec2bd555eabc0ed976c25
Jude95 <973829691@qq.com>
2015-04-03 17:51:06 +0800

    添加 Hello World

--- app/src/main/java/com/mredrock/date/MainActivity.java
+++ app/src/main/java/com/mredrock/date/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT);
     }
 
 
