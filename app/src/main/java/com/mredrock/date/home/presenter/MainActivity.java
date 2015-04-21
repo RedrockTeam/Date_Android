6571b5ce7c66822263d889c456cc5ceffc533789
Jude95 <973829691@qq.com>
2015-04-21 15:53:56 +0800

    test

---
+++ app/src/main/java/com/mredrock/date/home/presenter/MainActivity.java
@@ -0,0 +1,11 @@
+package com.mredrock.date.home.presenter;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Mr.Jude on 2015/4/21.
+ */
+public class MainActivity extends ActionBarActivity{
+
+
+}
