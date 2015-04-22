b0016d1dedb624e43386232ef8ae10acc38efb39
zhuchenxi <973829691@qq.com>
2015-04-22 23:49:38 +0800

    增加友盟意见反馈

--- app/src/main/java/com/mredrock/date/setting/presenter/FeedbackActivity.java
+++ app/src/main/java/com/mredrock/date/setting/presenter/FeedbackActivity.java
@@ -3,6 +3,7 @@ package com.mredrock.date.setting.presenter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mredrock.date.R;
 import com.umeng.analytics.MobclickAgent;
@@ -31,6 +32,10 @@ public class FeedbackActivity extends ActionBarActivity {
                     .add(R.id.container,mFeedbackFragment,"feedback")
                     .commit();
         }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void onResume() {
