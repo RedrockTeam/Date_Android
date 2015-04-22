51b977729508a0fdae6e8d248279ec43fa700e2a
Jude95 <973829691@qq.com>
2015-04-22 15:10:23 +0800

    修复bug

--- app/src/main/java/com/mredrock/date/app/APP.java
+++ app/src/main/java/com/mredrock/date/app/APP.java
@@ -3,6 +3,7 @@ package com.mredrock.date.app;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.mredrock.date.util.Utils;
 
 /**
  * Created by Mr.Jude on 2015/4/22.
@@ -11,6 +12,7 @@ public class APP extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Utils.initialize(this,"DateTag","0");
         Fresco.initialize(this);
     }
 }
