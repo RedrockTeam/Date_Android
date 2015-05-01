426e792aeb0aca9b1f12912c3dff7857ef8490ee
zhuchenxi <973829691@qq.com>
2015-05-01 14:50:19 +0800

    完成发布约

--- app/src/main/java/com/mredrock/date/home/presenter/EditActivityPresent.java
+++ app/src/main/java/com/mredrock/date/home/presenter/EditActivityPresent.java
@@ -1,5 +1,9 @@
 package com.mredrock.date.home.presenter;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.mredrock.date.R;
 import com.mredrock.date.app.BaseActivityPresenter;
 import com.mredrock.date.home.view.EditActivityVu;
 
@@ -7,6 +11,13 @@ import com.mredrock.date.home.view.EditActivityVu;
  * Created by zhuchenxi on 15/4/30.
  */
 public class EditActivityPresent extends BaseActivityPresenter<EditActivityVu> {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit,menu);
+        return true;
+    }
+
     @Override
     public Class<EditActivityVu> getVuClass() {
         return EditActivityVu.class;
--- app/src/main/java/com/mredrock/date/home/view/EditActivityVu.java
+++ app/src/main/java/com/mredrock/date/home/view/EditActivityVu.java
@@ -7,8 +7,10 @@ import com.mredrock.date.app.BaseActivityVu;
  * Created by zhuchenxi on 15/4/30.
  */
 public class EditActivityVu extends BaseActivityVu {
+
     @Override
     protected void onCreate() {
         setView(R.layout.activity_edit);
     }
+
 }
