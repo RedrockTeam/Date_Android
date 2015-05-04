7fae2b78ad7a9486d9a9eee0a7c64da8711dab9b
无名仕 <2469763120@qq.com>
2015-05-04 19:30:23 +0800

    text

---
+++ app/src/main/java/com/mredrock/date/detail/view/DetailActivityVu.java
@@ -0,0 +1,12 @@
+package com.mredrock.date.detail.view;
+
+import com.mredrock.date.R;
+import com.mredrock.date.app.BaseActivityVu;
+
+public class DetailActivityVu extends BaseActivityVu {
+
+    @Override
+    protected void onCreate() {
+        setView(R.layout.activity_detail);
+    }
+}
