aece492967b13d10e10ed86585dcf94edec03f0c
lecion <ylc931116@gmail.com>
2015-05-05 17:38:19 +0800

    完善Letter JavaBean

--- README.md
+++ README.md
@@ -51,12 +51,12 @@
             data : {
                 "user_id" : 123,
                 "user_name" : "Lecion",
-                "user_signature" : "个性签名",
+                "userSignature" : "个性签名",
                 "user_avatar" : "http://****.jpg",	//用户头像
                 "user_gender" : 2, 		//1是男，2是女
                 "content" : "约了我的炮",
                 "date_id" : 12,		//约会的id，只针对系统发送的含有约会的私信
                 "letter_status" : 1,	//私信状态，1 => 未读， 2 => 已读
-                "user_data_status" : 2,	//用户和约会的状态,0 => , 1 => 以接受, 2 => 默认（未处理）
+                "user_date_status" : 2,	//用户和约会的状态,0 => 已拒绝, 1 => 已接受, 2 => 默认（未处理）
             }
         }
--- app/src/main/java/com/mredrock/date/model/bean/Letter.java
+++ app/src/main/java/com/mredrock/date/model/bean/Letter.java
@@ -3,10 +3,102 @@ package com.mredrock.date.model.bean;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Lecion on 5/5/15.
  */
 public class Letter implements Parcelable{
+    @SerializedName("user_id")
+    private int userId;
+    @SerializedName("user_name")
+    private String userName;
+    @SerializedName("user_signature")
+    private String userSignature;
+    @SerializedName("user_avatar")
+    private String userAvatar;
+    @SerializedName("userGender")
+    private int userGender;
+    @SerializedName("content")
+    private String content;
+    @SerializedName("data_id")
+    private int dateId;
+    @SerializedName("letter_status")
+    private LetterStatus letterStatus;
+    @SerializedName("user_date_status")
+    private int userDateStatus;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSignature() {
+        return userSignature;
+    }
+
+    public void setUserSignature(String userSignature) {
+        this.userSignature = userSignature;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+
+    public int getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(int userGender) {
+        this.userGender = userGender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDateId() {
+        return dateId;
+    }
+
+    public void setDateId(int dateId) {
+        this.dateId = dateId;
+    }
+
+    public LetterStatus getLetterStatus() {
+        return letterStatus;
+    }
+
+    public void setLetterStatus(LetterStatus letterStatus) {
+        this.letterStatus = letterStatus;
+    }
+
+    public int getUserDateStatus() {
+        return userDateStatus;
+    }
+
+    public void setUserDateStatus(int userDateStatus) {
+        this.userDateStatus = userDateStatus;
+    }
 
     @Override
     public int describeContents() {
@@ -15,6 +107,30 @@ public class Letter implements Parcelable{
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-
     }
+
+    public enum LetterStatus {
+        UNREAD(1), READ(2);
+        private int value;
+
+        private LetterStatus(int value) {
+            this.value = value;
+        }
+
+        public int value() {
+            return this.value;
+        }
+
+        public static LetterStatus valueOf(int value) {
+            switch (value) {
+                case 1:
+                    return UNREAD;
+                case 2:
+                    return READ;
+                default:
+                    return null;
+            }
+        }
+    }
+
 }
