aedcd1dbad148d142c99e731f9dee497aed272b7
lecion <ylc931116@gmail.com>
2015-05-13 22:53:20 +0800

    增加私信详情页面的Toolbar按钮

--- app/src/main/java/com/mredrock/date/letter/presenter/LetterDetailActivityPresenter.java
+++ app/src/main/java/com/mredrock/date/letter/presenter/LetterDetailActivityPresenter.java
@@ -1,5 +1,9 @@
 package com.mredrock.date.letter.presenter;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mredrock.date.R;
 import com.mredrock.date.app.BaseActivityPresenter;
 import com.mredrock.date.letter.view.LetterDetailActivityVu;
 
@@ -18,5 +22,15 @@ public class LetterDetailActivityPresenter extends BaseActivityPresenter<LetterD
         super.onBindVu();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_letter_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
 }
